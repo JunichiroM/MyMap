a096efb4d7f325dda13abb134003705eb7b3d99e
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-09-19 17:20:35 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by apple on 2024/09/19.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
